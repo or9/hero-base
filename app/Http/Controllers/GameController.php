8b0f5e2b1b28a34255c5fd9bf0ac7da2a0a6b3ae
user0 <user0@>
2015-04-03 11:59:01 -0400

    routes, controllers

---
+++ app/Http/Controllers/GameController.php
@@ -0,0 +1,39 @@
+<? php namespace App\Http\Controllers;
+
+import Illuminate\Http\Response;
+
+class GameController extends Controller {
+
+	public function __construct()
+	{
+		return $this->middleware("user");
+	}
+
+	/**
+	 * @return Response
+	 */
+	public function characters() 
+	{
+		$response = DB::select("select * from characters");
+		return new Response($response);
+	}
+
+	/**
+	 * @return Response
+	 */
+	public function forms() 
+	{
+		$response = DB::select("select * from forms");
+		return new Response($response);
+	}
+
+	/**
+	 * @return Response
+	 */
+	public function scoreboard()
+	{
+		$response = DB::select("select * from scoreboard");
+		return new Response($response);
+	}
+
+}
